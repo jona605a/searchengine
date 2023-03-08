3e36fbe50358e634f0d43b51dba54502cce24e39
jona605a <jonathan.hoejlev@gmail.com>
2023-03-08 13:23:09 +0100

    :sparkles: Benchmarking index 8

--- rustsearch/benches/my_benchmark.rs
+++ rustsearch/benches/my_benchmark.rs
@@ -1,19 +1,28 @@
 // https://bheisler.github.io/criterion.rs/book/getting_started.html
-
+#[allow(unused_imports)]
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use rustsearch::fibonacci;
+use rustsearch::index;
 
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+// pub fn criterion_benchmark(c: &mut Criterion) {
+//     c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+// }
+
+pub fn indexing_8_file_100_kb(c: &mut Criterion) {
+    c.bench_function("index 8 indexing 100 KB", |b| b.iter(|| {
+        index::Index::index8_0(&rustsearch::helpers::Config {file_path : "data/WestburyLab.wikicorp.201004_100KB.txt".to_string(), indexno : "8_0".to_string()})
+    }));
+}
+
+pub fn indexing_8_file_5_mb(c: &mut Criterion) {
+    c.bench_function("index 8 indexing 5 MB", |b| b.iter(|| {
+        index::Index::index8_0(&rustsearch::helpers::Config {file_path : "data/WestburyLab.wikicorp.201004_5MB.txt".to_string(), indexno : "8_0".to_string()})
+    }));
 }
 
 
 
-
-
-
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, indexing_8_file_100_kb, indexing_8_file_5_mb);
 criterion_main!(benches);
 
