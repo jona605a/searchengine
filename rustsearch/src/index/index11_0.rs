d464803441a54ee835ea5b50d24ed14ee17503ea
jona605a <jonathan.hoejlev@gmail.com>
2023-05-03 18:04:57 +0200

    Fuzzy search

--- rustsearch/src/index/index11_0.rs
+++ rustsearch/src/index/index11_0.rs
@@ -1,5 +1,5 @@
 use regex::Regex;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 
 use crate::helpers::*;
@@ -58,11 +58,85 @@ impl Index<HashMap<(String, String, String), Vec<usize>>> {
         }
         output
     }
+
+    pub fn fuzzy_triples_search(&self, query: &String) -> Vec<String> {
+        // Split sentence into words
+        // Get article set for each word, and find intersection
+        let mut words_iter = query.split_ascii_whitespace();
+        let mut prv1 = match words_iter.next() {
+            None => return vec![],
+            Some(w) => w,
+        };
+        let mut prv2 = match words_iter.next() {
+            None => return vec![],
+            Some(w) => w,
+        };
+        let mut art_lists = vec![];
+
+        for word in words_iter {
+            let triple = (prv1.to_owned(), prv2.to_owned(), word.to_owned());
+            match dbg!(self.database.get(dbg!(&triple))) {
+                None => return vec![],
+                Some(al) => art_lists.push(al),
+            }
+            prv1 = prv2;
+            prv2 = word;
+        }
+        dbg!(&art_lists);
+
+        let mut art_iter = art_lists.iter();
+        let first_art = match art_iter.next() {
+            None => return vec![],
+            Some(&x) => x,
+        };
+
+        let art_intersect = art_lists.iter().fold(
+            HashSet::from_iter(first_art),
+            |acc: HashSet<&usize>, &art_lis| {
+                HashSet::from_iter(art_lis)
+                    .intersection(&acc)
+                    .map(|i| *i)
+                    .collect()
+            },
+        );
+        dbg!(&art_intersect);
+
+        // dbg!(&art_list);
+        // let mut x = art_lists.iter();
+
+        // let keys = match x.next() {
+        //     None => return vec![],
+        //     Some(&l) => l,
+        // }
+        // .clone();
+        // dbg!(&keys);
+        // dbg!(&x);
+
+        // // The intersection = all the numbers in the first list, that is also in all the others
+        // let art_intersect: Vec<&usize> = keys
+        //     .iter()
+        //     .filter(|&ar_no| x.all(|&hs_a| hs_a.contains(ar_no)))
+        //     .collect();
+
+        // dbg!(x.all(|hs_a| hs_a.contains(&1)));
+
+        dbg!(&art_intersect);
+
+        // panic!();
+        // Map from article numbers to titles
+        art_intersect
+            .iter()
+            .map(|&a_no| self.article_titles[*a_no].to_owned())
+            .collect()
+    }
 }
 
 impl Search for Index<HashMap<(String, String, String), Vec<usize>>> {
     fn search(&self, query: &Query) -> ArticleTitles {
-        todo!()
+        match &query.search_type {
+            SearchType::FuzzySearch => self.fuzzy_triples_search(&query.search_string),
+            _ => unimplemented!(),
+        }
     }
 }
 
@@ -124,48 +198,90 @@ mod tests {
         }
     }
 
-    #[test]
-    fn vec_to_articlelist_works() {
-        let test_index = setup_test();
-
-        let vec: Vec<usize> = vec![0, 1];
-
-        let hs = vec!["article 0".to_string(), "article 1".to_string()];
-        assert_eq!(test_index.vec_to_articlelist(vec), hs)
+    fn search_match(index: Index<HashMap<(String, String, String), Vec<usize>>>, query: Query, expected: Vec<String>) {
+        assert_eq!(
+            HashSet::from_iter(index.search(&query)),
+            HashSet::<String>::from_iter(expected)
+        )
     }
 
     #[test]
-    fn find_word() {
+    fn find_triple() {
         let index = setup_test();
-        let result = index
-            .database
-            .get(&(
-                "word2".to_string(),
-                "word3".to_string(),
-                "word4".to_string(),
-            ))
-            .unwrap();
-        assert_eq!(*result, vec![0, 1, 2, 3, 4, 5, 6, 7]);
+
+        let query = Query {
+            search_string: "word2 word3 word4".to_string(),
+            search_type: SearchType::FuzzySearch,
+        };
+        let result = index.search(&query);
+
+        assert_eq!(
+            *result,
+            vec![
+                "article 0",
+                "article 1",
+                "article 2",
+                "article 3",
+                "article 4",
+                "article 5",
+                "article 6",
+                "article 7"
+            ]
+        );
     }
 
     #[test]
     fn the_empty_query() {
         let index = setup_test();
-        let result = index
-            .database
-            .get(&("".to_string(), "".to_string(), "".to_string()));
-        assert_eq!(result, None);
+
+        let query = Query {
+            search_string: "".to_string(),
+            search_type: SearchType::FuzzySearch,
+        };
+        let result = index.search(&query);
+
+        assert_eq!(*result, Vec::<String>::new());
+
+        let query = Query {
+            search_string: "hej".to_string(),
+            search_type: SearchType::FuzzySearch,
+        };
+        let result = index.search(&query);
+
+        assert_eq!(*result, Vec::<String>::new());
+
+        let query = Query {
+            search_string: "hej med".to_string(),
+            search_type: SearchType::FuzzySearch,
+        };
+        let result = index.search(&query);
+
+        assert_eq!(*result, Vec::<String>::new());
     }
 
     #[test]
     fn erroneous_query_finds_nothing() {
         let index = setup_test();
-        let result = index.database.get(&(
-            "word4".to_string(),
-            "word5".to_string(),
-            "word3".to_string(),
-        ));
-        assert_eq!(result, None);
+
+        let query = Query {
+            search_string: "word4 word5 word3".to_string(),
+            search_type: SearchType::FuzzySearch,
+        };
+        let result = index.search(&query);
+
+        assert_eq!(*result, Vec::<String>::new());
+    }
+
+    #[test]
+    fn more_than_three_words() {
+        let index = setup_test();
+
+        let query = Query {
+            search_string: "word2 word3 word4 word5".to_string(),
+            search_type: SearchType::FuzzySearch,
+        };
+
+        search_match(index, query, vec!["article 0".to_string(), "article 2".to_string(), "article 4".to_string(), "article 6".to_string()]);
     }
 
     #[test]
