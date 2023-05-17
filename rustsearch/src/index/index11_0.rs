f95d45a14afde34e41d6ffa310f08a2ff701d356
FrederikkeUldahl <frederikkeu@gmail.com>
2023-05-17 16:31:03 +0200

    index 11 does not work

--- rustsearch/src/index/index11_0.rs
+++ rustsearch/src/index/index11_0.rs
@@ -1,4 +1,3 @@
-use regex::Regex;
 use std::collections::{HashMap, HashSet};
 use std::error::Error;
 
@@ -11,19 +10,12 @@ impl Index<HashMap<(String, String, String), Vec<usize>>> {
     pub fn index11(config: &Config) -> Result<Self, Box<dyn Error>> {
         let mut database: HashMap<(String, String, String), Vec<usize>> = HashMap::new();
 
-        let filecontents = read_file_to_string(&config.file_path)?;
-        let re = Regex::new(r"\. |\.\n|\n\n|; |[\[\]\{\}\\\n\(\) ,:/=?!*]").unwrap();
-
-        // Articles are seperated by the delimiter "---END.OF.DOCUMENT---"
-        // In each article, it is assumed that the first line is the title, ending in a '.'
-        // The contents of each article is split according to the regular expression.
-        let articles_iter = filecontents.split("---END.OF.DOCUMENT---").map(|a| {
-            let (title, contents) = a.trim().split_once(".\n").unwrap_or(("", ""));
-            (title.to_string(), re.split(contents))
-        });
+        let articles_iter = read_and_clean_file_to_iter(config)?;
+        
         let mut article_titles: Vec<String> = Vec::new();
 
-        for (title, mut contents) in articles_iter {
+        for (title, contents) in articles_iter {
+            let mut contents = contents.iter();
             if title != "" {
                 article_titles.push(title.to_string());
 
